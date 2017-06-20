c1f1418d711d63c35db21561eb2513794500db68
lucascoding <lucas@personalwebzone.com>
2017-06-20 09:34:45 -0700

    created Unit Test project

---
+++ Template_Angular2_JwtAuthorization.UT/SampleTestClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template_Angular2_JwtAuthorization.UT
+{
+    [TestClass]
+    public class SampleTestClass
+    {
+        [TestMethod]
+        public void SampleTestMethod()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
