d8e3b35938946094a9cf6667a27a81047defd091
lucascoding <lucas@personalwebzone.com>
2017-06-19 19:09:53 -0700

    first commit

---
+++ Template_Angular2_JwtAuthorization.UI/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TePage } from './app.po';
+
+describe('te App', () => {
+  let page: TePage;
+
+  beforeEach(() => {
+    page = new TePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
